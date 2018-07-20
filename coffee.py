0d4806fc3438b0ed9b6c4a200c8d02b1e012a9ea
mrgerwin <jgerwin@otsegoknights.org>
2018-07-20 13:25:37 -0400

    finished steps

--- coffee.py
+++ coffee.py
@@ -2,6 +2,7 @@ def printReceipt(name, coffeeNum, hChocolateNum, totalCost):
   print("*************************************")
   print("Thank you for purchasing from Knights Coffee Shop " + name)
   print(" ")
+  print("Your Cashier was Joe Shmoe")
   print("Coffees..............." + str(coffeeNum)+" x 0.25")
   print("Hot Chocolates........" + str(hChocolateNum) + " x 0.50")
   print("----------------------------------")
@@ -12,3 +13,8 @@ hChocolateNum = 2
 totalCost = 1.75
 printReceipt(name, coffeeNum, hChocolateNum, totalCost)
 #Added student work here
+
+printReceipt("Mr. O'Shea", 20,0,5.00)
+printReceipt("Ms. Jones", 0, 1, .50)
+printReceipt("Mr. Jeffers", 1, 1, .75)
+
